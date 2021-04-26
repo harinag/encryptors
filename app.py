459bb31f7b611e1fd711b64e476529fa2eefdf66
Max Huzmiev <max@huzm.ru>
2021-04-26 22:31:06 +0300

    Encrypt algorithm now works

--- app.py
+++ app.py
@@ -30,5 +30,37 @@ def calc():
     return '; '.join(res)
 
 
+@app.route('/encrypt', methods=['GET'])
+def encrypt():
+    thetext = request.args.get('theText')
+    thekey = request.args.get('theKey')
+    # Check input values
+    if len(thetext) < 1:
+        return 'Не задан исходный текст'
+    if len(thekey) < 1:
+        return 'Не задан ключ шифрования'
+    thekey = int(thekey)
+    if thekey < -32 or thekey > 32:
+        return 'Неверное значение ключа'
+    # Values OK
+    alpb1 = 'АБВГДЕЁЖЗИЙКЛМНОПРСТУФХЦЧШЩЪЫЬЭЮЯАБВГДЕЁЖЗИЙКЛМНОПРСТУФХЦЧШЩЪЫЬЭЮЯ'
+    alpb2 = 'абвгдеёжзийклмнопрстуфхцчшщъыьэюяабвгдеёжзийклмнопрстуфхцчшщъыьэюя'
+    ext = 0
+    if thekey < 0: ext = 33
+    res = ''
+    for ltr in thetext:
+        enc = ''
+        i1 = alpb1.find(ltr)
+        i2 = alpb2.find(ltr)
+        if i1 >= 0:
+            enc = alpb1[i1 + ext + thekey]
+        elif i2 >= 0:
+            enc = alpb2[i2 + ext + thekey]
+        else:
+            enc = ' '
+        res += enc
+    return '<p>Результат:</p><b>' + res + '</b>'
+
+
 if __name__ == '__main__':
     app.run(debug=True, port=5000)
